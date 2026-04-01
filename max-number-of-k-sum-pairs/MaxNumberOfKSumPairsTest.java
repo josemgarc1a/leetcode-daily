652bf2feb51ba2a079db84e5c8b1c4177e6bdfac
Jose Garcia <josemgarc1a@icloud.com>
2026-04-01 07:54:11 -0500

    test:Max Number of K Sum Pairs

--- max-number-of-k-sum-pairs/MaxNumberOfKSumPairsTest.java
+++ max-number-of-k-sum-pairs/MaxNumberOfKSumPairsTest.java
@@ -1,3 +1,43 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class MaxNumberOfKSumPairsTest {
-    
+
+    private final MaxNumberOfKSumPairs solver = new MaxNumberOfKSumPairs();
+
+    @Test
+    void example1() {
+        assertEquals(2, solver.maxOperations(new int[] {1, 2, 3, 4}, 5));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(1, solver.maxOperations(new int[] {3, 1, 3, 4, 3}, 6));
+    }
+
+    @Test
+    void returnsZeroWhenNoPairsSumToK() {
+        assertEquals(0, solver.maxOperations(new int[] {1, 2, 3}, 100));
+    }
+
+    @Test
+    void handlesAllPairsFormable() {
+        assertEquals(2, solver.maxOperations(new int[] {2, 2, 2, 2}, 4));
+    }
+
+    @Test
+    void handlesDuplicatesWithLimitedComplements() {
+        assertEquals(2, solver.maxOperations(new int[] {1, 1, 2, 2, 3, 3}, 4));
+    }
+
+    @Test
+    void handlesSinglePairOnly() {
+        assertEquals(1, solver.maxOperations(new int[] {5, 1}, 6));
+    }
+
+    @Test
+    void handlesZeros() {
+        assertEquals(2, solver.maxOperations(new int[] {0, 0, 0, 0}, 0));
+    }
 }
